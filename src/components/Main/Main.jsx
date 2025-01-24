a35e3e4b87589173ae3a3df4dda168befac5fd70
Chris Kapano <kapanochris@yahoo.com>
2025-01-24 14:57:32 -0800

    Update weather card and main component to toggle between F and C

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,14 +2,19 @@ import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import { defaultClothingItems } from "../../utils/constants";
 import "./Main.css";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
+import { useContext } from "react";
 
 function Main({ weatherData, handleCardClick }) {
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+
   return (
     <main>
       <WeatherCard weatherData={weatherData} />
       <section className="cards">
         <p className="cards__text">
-          Today is {weatherData.temp.F} &deg; F / You may want to wear:
+          Today is {weatherData.temp[currentTemperatureUnit]}°
+          {[currentTemperatureUnit]} / You may want to wear:
         </p>
         <ul className="cards__list">
           {defaultClothingItems
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,10 +1,16 @@
 import "./WeatherCard.css";
 import sunny from "../../assets/sunny.png";
+import { useContext } from "react";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 
 function WeatherCard({ weatherData }) {
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
+      <p className="weather-card__temp">
+        {weatherData.temp[currentTemperatureUnit]}°{currentTemperatureUnit}
+      </p>
       <img className="weather-card__image" src={sunny} alt="sunny" />
     </section>
   );
